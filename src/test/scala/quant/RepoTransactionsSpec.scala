2c1581d82e344e72b83847cd6500994cbe71feed
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-14 19:21:43 +0100

    knowing a what is returned from the csv reading process

--- src/test/scala/quant/RepoTransactionsSpec.scala
+++ src/test/scala/quant/RepoTransactionsSpec.scala
@@ -2,8 +2,23 @@ package quant
 
 import org.scalatest.FunSuite
 
-class RepoTransactionsSpec extends FunSuite {
-  test("can read a csv") {
+import scala.io.Source
 
+class RepoTransactionsSpec extends FunSuite {
+
+  test("can get URL of real file") {
+    val file = getClass.getResource("/transactions.txt")
+
+    // this test will fail in any other environment as is not environment agnostic.
+    // A classic solution to this issue is creating a defined environment using docker, virtualbox, etc.
+    assert("/Users/tati/Desktop/scala_lab/quant/target/scala-2.12/classes/transactions.txt" == file.getPath)
+  }
+
+  test("Can really read a file using the provided code") {
+    val file = getClass.getResource("/transactions.txt")
+    val transactionslines = Source.fromFile(file.getPath).getLines().drop(1).toList
+
+    assert(991 == transactionslines.length)
+    assert("T0001,A27,1,GG,338.11" == transactionslines(0))
   }
 }
