fef4311e5749d86a4d625ab6b30f2b8225ee983d
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-20 16:29:57 +0100

    question 3 functinal

--- src/main/scala/quant/Question3.scala
+++ src/main/scala/quant/Question3.scala
@@ -1,13 +1,19 @@
 package quant
 
+import cats.data.EitherT
 import cats.effect._
 import cats.syntax.all._
+import quant.Question2.{AccCatMapStat, analyze}
+import quant.RepositoryTransactions.ListTransaction
+
 import scala.annotation.tailrec
 
 object Question3 extends IOApp {
 
+  type WindowMapStat = Map[OpTransactions.KeyWindow, StatByWindowAcc]
+
   @tailrec
-  def analyze(transactions: List[Transaction], statsAcumulator: Map[OpTransactions.KeyWindow, StatByWindowAcc] = Map()): Map[OpTransactions.KeyWindow, StatByWindowAcc]  = {
+  def analyze(transactions: List[Transaction], statsAcumulator: WindowMapStat = Map()): WindowMapStat = {
     transactions match {
       case Nil => statsAcumulator
       case _ => analyze(
@@ -17,13 +23,15 @@ object Question3 extends IOApp {
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
-//    val transactions: List[Transaction] = RepositoryTransactions.findAll("/transactions.txt")
-//    val stats = analyze(transactions.reverse)
-//    val tableText = Render.run(stats.values.toList.sortBy(_.day),
-//      """
-//        |Day|Account|Max|Avg|AAcat|CCcat|FFcat""".stripMargin)
+    val transactions: IO[Either[ErrorRead, ListTransaction]] = RepositoryTransactions.findAll("/transactions.txt")
 
+    val stats: IO[Either[ErrorRead, WindowMapStat]] = EitherT(transactions).map(analyze(_)).value
 
+    val tableText = EitherT(stats).map((x: WindowMapStat) =>
+      Render.run(x.values.toList.sortBy(_.day), "\n|Day|Account|Max|Avg|AAcat|CCcat|FFcat\n")
+    ).value
+
+    println(tableText.unsafeRunSync())
     IO{println("tableText")}.as(ExitCode.Success)
   }
 }
