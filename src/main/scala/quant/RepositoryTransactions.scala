8b746b6441275e88a1c5787633f449f41ec30d06
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-20 15:03:27 +0100

    more

--- src/main/scala/quant/RepositoryTransactions.scala
+++ src/main/scala/quant/RepositoryTransactions.scala
@@ -1,15 +1,27 @@
 package quant
 
+import cats.effect.IO
 import scala.io.Source
+import cats.implicits._
+import scala.util.{Success, Try}
 
 object RepositoryTransactions {
 
-  def findAll(resourceFilename: String): List[Transaction] = {
+  type ListTransaction = List[Transaction]
+
+  def findAll(resourceFilename: String): IO[Either[ErrorRead, ListTransaction]] = IO {
     val file = getClass.getResource(resourceFilename)
 
-    for {
-      line <- Source.fromFile(file.getPath).getLines().drop(1).toList
-      fields = line.split(',')
-    } yield Transaction(fields(0), fields(1), fields(2).toInt, fields(3), fields(4).toDouble)
+    val transactions: Try[List[Transaction]] = Try {
+      for {
+        line <-  Source.fromFile(file.getPath).getLines().drop(1).toList
+        fields = line.split(',')
+      } yield Transaction(fields(0), fields(1), fields(2).toInt, fields(3), fields(4).toDouble)
+    }
+
+    transactions match {
+      case Success(transactions) => transactions.asRight[ErrorRead]
+      case _ => FileDontExist.asLeft[ListTransaction]
+    }
   }
 }
--- src/test/scala/quant/RepositoryTransactionsSpec.scala
+++ src/test/scala/quant/RepositoryTransactionsSpec.scala
@@ -1,35 +1,22 @@
 package quant
 
 import org.scalatest.FunSuite
-
-import scala.io.Source
+import quant.RepositoryTransactions.ListTransaction
 
 class RepositoryTransactionsSpec extends FunSuite {
-//  test("Can really read a file using the provided code") {
-//    val file = getClass.getResource("/transactions.txt")
-//    val transactionslines = Source.fromFile(file.getPath).getLines().drop(1).toList
-//
-//    assert(991 == transactionslines.length)
-//    assert("T0001,A27,1,GG,338.11" == transactionslines(0))
-//  }
-//
-//  test("Can read transactions") {
-//    val result: Either[ErrorRead, ListTransaction] = RepositoryTransactions
-//      .findAll("/transactions.txt")
-//      .unsafeRunSync()
-//
-//    assert(result.isRight)
-//
-//    for{
-//      listTransactions <- result
-//    } yield assert(listTransactions.length == 991)
-//  }
-//
-//  test("Cannot read transactions") {
-//    val result: Either[ErrorRead, ListTransaction] = RepositoryTransactions
-//      .findAll("/unknownfile.txt")
-//      .unsafeRunSync()
-//
-//    assert(result.isLeft)
-//  }
+  test("Right if we can read the file") {
+    val result: Either[ErrorRead, ListTransaction] = RepositoryTransactions
+      .findAll("/transactions.txt")
+      .unsafeRunSync()
+
+    assert(result.isRight)
+  }
+
+  test("Is left if file cannot be read") {
+    val result: Either[ErrorRead, ListTransaction] = RepositoryTransactions
+      .findAll("/unknownfile.txt")
+      .unsafeRunSync()
+
+    assert(result.isLeft)
+  }
 }
